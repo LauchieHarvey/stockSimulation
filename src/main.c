6925eff2b7af2399542fcc59eb392dafaaa8f8a3
LauchieHarvey <lauchieharvey@gmail.com>
2020-11-28 17:35:28 +1000

    Freeing memory allocated in run_simulation. All memory is now freed.

--- src/main.c
+++ src/main.c
@@ -77,6 +77,7 @@ void* run_simulation(void* voidArgs) {
     push(args->stack, totalAccountValue);
     printf("totAccVal: %f\n", totalAccountValue);
     fflush(stdout);
+    free(stock.prices);
     free_account(account);
 
     return NULL;
